2dc631013b2c4d75c081d5ac0eab95d2c79f40f3
Marion Vizier-Marzais <marionviz@hotmail.com>
2025-12-11 00:06:30 +0100

    textintro

--- src/App.tsx
+++ src/App.tsx
@@ -167,7 +167,7 @@ function App() {
         const welcomeMessage: ChatMessage = {
           id: `model-${Date.now()}`,
           role: 'model',
-          text: `Bonjour ! Je suis l'avatar de Marion. Mon objectif est de vous aider à pratiquer votre écrit en appliquant ce que vous apprenez en cours. Nous sommes en semaine ${week}. Commençons à pratiquer ! Comment allez-vous aujourd'hui ?`,
+          text: `Bonjour ! Je suis l'avatar de Marion. Mon objectif est de vous aider à pratiquer votre écrit en utilisant ce que vous apprenez en cours. Nous sommes en semaine ${week}. Commençons à pratiquer, c'est d'accord ?`,
         };
         setMessages([welcomeMessage]);
         setIsLoading(false);
