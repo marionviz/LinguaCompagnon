39da28fe9449346fb4fe00e0d213b59770653010
Marion Vizier-Marzais <marionviz@hotmail.com>
2025-12-15 10:07:52 +0100

    appmobile

---
+++ public/sw.js
@@ -0,0 +1,24 @@
+// Service Worker basique pour mise en cache
+const CACHE_NAME = 'linguacompagnon-v1';
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
